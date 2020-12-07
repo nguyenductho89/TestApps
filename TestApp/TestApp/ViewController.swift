340ca9e98a06c64daf7acfa9927c10796ba0418c
ThoND <nguyenductho89@gmail.com>
2020-12-07 20:27:44 +0700

    init

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Nguyễn Đức Thọ on 12/7/20.
+//  Copyright © 2020 thond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
