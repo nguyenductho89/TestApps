707621b322ccf9f16b7351680221affc78874952
nguyenductho89 <nguyenductho89@gmail.com>
2021-01-09 15:11:09 +0700

    add doreamonkit 2

--- TestApp/TestApp/IBDesignable/Tableview/TestViewController.swift
+++ TestApp/TestApp/IBDesignable/Tableview/TestViewController.swift
@@ -9,10 +9,52 @@
 import UIKit
 
 class TestViewController: UIViewController {
-
+    let second = SecondTestViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "TestViewController"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(toSecond))
+        // Do any additional setup after loading the view.
+    }
 
+    @objc func toSecond() {
+        
+        second.delegate = self
+        self.navigationController?.pushViewController(second, animated: false)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+class SecondTestViewController: UIViewController{
+
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//        /// ...
+//        trackLifetime()
+//    }
+    var delegate: UIViewController?
+    let c = C()
+    let d = D()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "SecondTestViewController"
+        let a = A()
+        let b = B()
+        a.b = b
+        b.a = a
+        
+        c.d = d
+        d.c = c
         // Do any additional setup after loading the view.
     }
 
@@ -27,4 +69,20 @@ class TestViewController: UIViewController {
     }
     */
 
+}
+
+class C {
+    var d: D?
+}
+
+class D {
+    var c: C?
+}
+
+class A {
+    var b: B?
+}
+
+class B {
+    var a: A?
 }
--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Main"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(toNil))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(toFirst))
         // Do any additional setup after loading the view.
     }
 
+    @objc func toFirst() {
+        let first = TestViewController()
+        self.navigationController?.pushViewController(first, animated: false)
+    }
+    
+    @objc func toNil() {
+
+        //self.first.releaseMe()
+            
+        
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         
@@ -26,3 +40,20 @@ class ViewController: UIViewController {
 
 }
 
+extension NSObjectProtocol {
+  /// Same as retain(), which the compiler no longer lets us call:
+  @discardableResult
+  func retainMe() -> Self {
+    _ = Unmanaged.passRetained(self)
+    return self
+  }
+
+  /// Same as autorelease(), which the compiler no longer lets us call.
+  ///
+  /// This function does an autorelease() rather than release() to give you more flexibility.
+  @discardableResult
+  func releaseMe() -> Self {
+    _ = Unmanaged.passUnretained(self).autorelease()
+    return self
+  }
+}
