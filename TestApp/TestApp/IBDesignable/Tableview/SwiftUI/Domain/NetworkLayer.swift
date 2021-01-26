84c304150d98822db9ae27e4d20d3d2cb1405ba1
nguyenductho89 <nguyenductho89@gmail.com>
2021-01-26 19:34:48 +0700

    tmp

--- TestApp/TestApp/IBDesignable/Tableview/SwiftUI/Domain/NetworkLayer.swift
+++ TestApp/TestApp/IBDesignable/Tableview/SwiftUI/Domain/NetworkLayer.swift
@@ -178,24 +178,3 @@ struct MovieSource: DataModelSource {
 enum DataClientError: Error {
     case decode
 }
-
-
-
-struct RestApiClient {
-
-    struct Response<T> {
-        let value: T
-        let response: URLResponse
-    }
-    
-    func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<Response<T>, Error> { // 2
-        return URLSession.shared
-            .dataTaskPublisher(for: request) // 3
-            .tryMap { result -> Response<T> in
-                let value = try JSONDecoder().decode(T.self, from: result.data) // 4
-                return Response(value: value, response: result.response) // 5
-            }
-            .receive(on: DispatchQueue.main) // 6
-            .eraseToAnyPublisher() // 7
-    }
-}
