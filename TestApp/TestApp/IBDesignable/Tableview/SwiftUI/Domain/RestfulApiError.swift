e759c1acca662fc42c31482744e15e2b2ee78070
nguyenductho89 <nguyenductho89@gmail.com>
2021-01-31 23:42:50 +0700

    add task

--- TestApp/TestApp/IBDesignable/Tableview/SwiftUI/Domain/RestfulApiError.swift
+++ TestApp/TestApp/IBDesignable/Tableview/SwiftUI/Domain/RestfulApiError.swift
@@ -12,7 +12,16 @@ import Foundation
 //        case 403: return .failure(FRNetworkingError.forbidden)
 //        case 404...500: return .failure(FRNetworkingError.authenticationError)
 //        case 501...599: return .failure(FRNetworkingError.badRequest)
-
+/*
+    1 REfresh token
+ */
 //https://gist.github.com/saroar/ca78de9dc798cdbaaa47791380062596
 //https://stackoverflow.com/questions/59719270/refresh-token-with-swiftui-combine
 //https://www.avanderlee.com/swift/authentication-alamofire-request-adapter/
+
+/*
+    2 Cache: memory, disk
+ */
+//https://blog.danlew.net/2015/06/22/loading-data-from-multiple-sources-with-rxjava/
+//https://medium.com/yammer-engineering/chaining-multiple-sources-with-rxjava-20eb6850e5d9
+
