f12dc89174c879743ccd5c7a29891453ff43cb2a
nguyenductho <nguyenductho89@ominext.com>
2021-02-01 08:55:43 +0700

    Add tasks

---
+++ TestApp/TestApp/Tasks/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  TestApp
+//
+//  Created by Nguyen Duc Tho on 2/1/21.
+//  Copyright © 2021 thond. All rights reserved.
+//
+
+import Foundation
+
+/*
+    1 REfresh token
+ */
+//https://gist.github.com/saroar/ca78de9dc798cdbaaa47791380062596
+//https://stackoverflow.com/questions/59719270/refresh-token-with-swiftui-combine
+//https://www.avanderlee.com/swift/authentication-alamofire-request-adapter/
+
+/*
+    2 Cache: memory, disk
+ */
+//https://blog.danlew.net/2015/06/22/loading-data-from-multiple-sources-with-rxjava/
+//https://medium.com/yammer-engineering/chaining-multiple-sources-with-rxjava-20eb6850e5d9
+/*
+    3 Flow coordinator
+ */
+//https://medium.com/flawless-app-stories/flow-coordinator-pattern-on-steroids-a52021e31bfe
+
+/*
+    4 Restful api error
+ */
